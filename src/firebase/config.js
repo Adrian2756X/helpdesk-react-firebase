598a12af54a2c8e0870f4819447694cc95a4f52c
Adrian2756X <adrian080801@gmail.com>
2026-07-12 21:06:07 -0600

    chore: ocultar credenciales de firebase en archivo .env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,13 +9,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCyGUgmHiT4wpeSerIUtiF6j4lwJA0XQHY",
-  authDomain: "helpdesk-soporte-bce91.firebaseapp.com",
-  projectId: "helpdesk-soporte-bce91",
-  storageBucket: "helpdesk-soporte-bce91.firebasestorage.app",
-  messagingSenderId: "914882909467",
-  appId: "1:914882909467:web:f6705a77167a54082befbe",
-  measurementId: "G-XN8FM5612J"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
